c4d56e9b95b4400f8ffb32591baf1118fee2271d
VachanAnand <anand.vachan15@gmail.com>
2020-04-17 06:47:39 +1000

    Update search_vet.php

--- search_vet.php
+++ search_vet.php
@@ -1,1 +1,31 @@
-hey
+
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+
+
+
+</html>
+
+<?php
+/*
+Template Name: new
+*/
+
+
+echo "Hey Boss\nhow are you";
+
+echo "hwy";
+
+echo (int) 9.9 ;
+echo '\'';
+
+
+?>
